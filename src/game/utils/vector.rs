2c57ee9e5d028160ae16397f977f554e1e42ff7e
bstriker <strikeforcezero@gmail.com>
2025-06-05 19:28:07 -0400

    refactor(util): more utility

--- src/game/utils/vector.rs
+++ src/game/utils/vector.rs
@@ -1,5 +1,3 @@
-use crate::game::utils::extensions::vec2::Vec2Ext;
-use crate::game::utils::extensions::vec3::Vec3Ext;
 use bevy::prelude::*;
 use bevy_auto_plugin::auto_plugin::*;
 
@@ -9,6 +7,18 @@ use bevy_auto_plugin::auto_plugin::*;
 pub struct XY_2D(pub Vec2);
 
 impl XY_2D {
+    pub const fn new(x: f32, y: f32) -> Self {
+        Self(Vec2::new(x, y))
+    }
+    const fn inner(self) -> Vec2 {
+        self.0
+    }
+    pub const fn x(self) -> f32 {
+        self.inner().x
+    }
+    pub const fn y(self) -> f32 {
+        self.inner().y
+    }
     pub const fn to_vector(self) -> Vector {
         Vector::XY_2D(self)
     }
@@ -20,12 +30,27 @@ impl From<XY_2D> for Vector {
     }
 }
 
+impl From<Vec2> for XY_2D {
+    fn from(vec2: Vec2) -> Self {
+        Self(vec2)
+    }
+}
+
 #[allow(non_camel_case_types)]
 #[auto_register_type]
 #[derive(Debug, Default, Copy, Clone, PartialEq, Reflect)]
 pub struct XZ_3D(pub Vec2);
 
 impl XZ_3D {
+    pub const fn new(x: f32, z: f32) -> Self {
+        Self(Vec2::new(x, z))
+    }
+    pub const fn x(self) -> f32 {
+        self.to_vector().to_bevy_3d().inner().x
+    }
+    pub const fn z(self) -> f32 {
+        self.to_vector().to_bevy_3d().inner().z
+    }
     pub const fn to_vector(self) -> Vector {
         Vector::XZ_3D(self)
     }
@@ -43,6 +68,15 @@ impl From<XZ_3D> for Vector {
 pub struct YZ_3D(pub Vec2);
 
 impl YZ_3D {
+    pub const fn new(y: f32, z: f32) -> Self {
+        Self(Vec2::new(y, z))
+    }
+    pub const fn y(self) -> f32 {
+        self.to_vector().to_bevy_3d().inner().y
+    }
+    pub const fn z(self) -> f32 {
+        self.to_vector().to_bevy_3d().inner().z
+    }
     pub const fn to_vector(self) -> Vector {
         Vector::YZ_3D(self)
     }
@@ -60,6 +94,15 @@ impl From<YZ_3D> for Vector {
 pub struct XY_3D(pub Vec2);
 
 impl XY_3D {
+    pub const fn new(x: f32, y: f32) -> Self {
+        Self(Vec2::new(x, y))
+    }
+    pub const fn x(self) -> f32 {
+        self.to_vector().to_bevy_3d().inner().x
+    }
+    pub const fn y(self) -> f32 {
+        self.to_vector().to_bevy_3d().inner().y
+    }
     pub const fn to_vector(self) -> Vector {
         Vector::XY_3D(self)
     }
@@ -77,6 +120,30 @@ impl From<XY_3D> for Vector {
 pub struct XYZ_3D(pub Vec3);
 
 impl XYZ_3D {
+    pub const fn new(x: f32, y: f32, z: f32) -> Self {
+        Self(Vec3::new(x, y, z))
+    }
+    const fn inner(self) -> Vec3 {
+        self.0
+    }
+    pub const fn x(self) -> f32 {
+        self.to_vector().to_bevy_3d().inner().x
+    }
+    pub const fn y(self) -> f32 {
+        self.to_vector().to_bevy_3d().inner().y
+    }
+    pub const fn z(self) -> f32 {
+        self.to_vector().to_bevy_3d().inner().z
+    }
+    pub const fn xy(self) -> XY_3D {
+        XY_3D::new(self.x(), self.y())
+    }
+    pub const fn yz(self) -> YZ_3D {
+        YZ_3D::new(self.y(), self.z())
+    }
+    pub const fn xz(self) -> XZ_3D {
+        XZ_3D::new(self.x(), self.z())
+    }
     pub const fn to_vector(self) -> Vector {
         Vector::XYZ_3D(self)
     }
@@ -85,6 +152,12 @@ impl XYZ_3D {
 impl From<XYZ_3D> for Vector {
     fn from(xyz_3d: XYZ_3D) -> Self {
         Self::XYZ_3D(xyz_3d)
+    }
+}
+
+impl From<Vec3> for XYZ_3D {
+    fn from(vec3: Vec3) -> Self {
+        Self(vec3)
     }
 }
 
@@ -153,11 +226,11 @@ mod tests {
     const Z: f32 = 3.0;
 
     fn d2(x: f32, y: f32) -> XY_2D {
-        XY_2D(Vec2::new(x, y))
+        XY_2D::new(x, y)
     }
 
     fn d3(x: f32, y: f32, z: f32) -> XYZ_3D {
-        XYZ_3D(Vec3::new(x, y, z))
+        XYZ_3D::new(x, y, z)
     }
 
     fn vec2(x: f32, y: f32) -> Vec2 {
@@ -170,18 +243,18 @@ mod tests {
 
     #[test]
     fn to_bevy_2d() {
-        assert_eq!(XY_2D(vec2(X, Y)).to_vector().to_bevy_2d(), d2(X, Y));
-        assert_eq!(XZ_3D(vec2(X, Z)).to_vector().to_bevy_2d(), d2(X, Z));
-        assert_eq!(YZ_3D(vec2(Y, Z)).to_vector().to_bevy_2d(), d2(EMPTY, -Z));
-        assert_eq!(XY_3D(vec2(X, Y)).to_vector().to_bevy_2d(), d2(X, EMPTY));
-        assert_eq!(XYZ_3D(vec3(X, Y, Z)).to_vector().to_bevy_2d(), d2(X, -Z));
+        assert_eq!(XY_2D::new(X, Y).to_vector().to_bevy_2d(), d2(X, Y));
+        assert_eq!(XZ_3D::new(X, Z).to_vector().to_bevy_2d(), d2(X, Z));
+        assert_eq!(YZ_3D::new(Y, Z).to_vector().to_bevy_2d(), d2(EMPTY, -Z));
+        assert_eq!(XY_3D::new(X, Y).to_vector().to_bevy_2d(), d2(X, EMPTY));
+        assert_eq!(XYZ_3D::new(X, Y, Z).to_vector().to_bevy_2d(), d2(X, -Z));
     }
 
     #[test]
     fn to_bevy_3d() {
-        assert_eq!(XY_2D(vec2(X, Y)).to_vector().to_bevy_3d(), d3(X, EMPTY, -Y));
-        assert_eq!(XZ_3D(vec2(X, Z)).to_vector().to_bevy_3d(), d3(X, EMPTY, Z));
-        assert_eq!(XY_3D(vec2(X, Y)).to_vector().to_bevy_3d(), d3(X, Y, EMPTY));
-        assert_eq!(XYZ_3D(vec3(X, Y, Z)).to_vector().to_bevy_3d(), d3(X, Y, Z));
+        assert_eq!(XY_2D::new(X, Y).to_vector().to_bevy_3d(), d3(X, EMPTY, -Y));
+        assert_eq!(XZ_3D::new(X, Z).to_vector().to_bevy_3d(), d3(X, EMPTY, Z));
+        assert_eq!(XY_3D::new(X, Y).to_vector().to_bevy_3d(), d3(X, Y, EMPTY));
+        assert_eq!(XYZ_3D::new(X, Y, Z).to_vector().to_bevy_3d(), d3(X, Y, Z));
     }
 }
