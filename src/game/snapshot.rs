9c47bc7951fadedb06c727b4a1d40fddef202a4b
bstriker <strikeforcezero@gmail.com>
2025-06-05 21:07:40 -0400

    refactor: restore Snapshot Default impl

--- src/game/snapshot.rs
+++ src/game/snapshot.rs
@@ -1,9 +1,15 @@
 use bevy::prelude::*;
 use bevy_auto_plugin::auto_plugin::*;
 
-#[derive(Component, Debug, Default, Reflect)]
+#[derive(Component, Debug, Reflect)]
 #[reflect(Component)]
 pub struct Snapshot<T>(Option<T>);
+
+impl<T> Default for Snapshot<T> {
+    fn default() -> Self {
+        Self(None)
+    }
+}
 
 impl<T> Snapshot<T> {
     pub fn replace(&mut self, new: T) -> Option<T> {
